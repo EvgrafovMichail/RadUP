caa97fdf333d3b43f8119f67924f4ca1f28f597a
EvgrafovMichail <evgrafovmd@gmail.com>
2023-08-21 23:33:20 +0300

    create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <string>
+#include <queue>
+#include <mutex>
+
+#include "cpp_modules/worker_functions/worker_functions.h"
+
+
+int main()
+{
+    std::queue<std::string> data_queue;
+    std::mutex data_queue_mutex;
+
+    std::thread speaker(startRadarSimulatorWorker, "127.0.0.1", 8000);
+    std::thread listener(
+        startListenerWorker, 8000,
+        std::ref(data_queue), std::ref(data_queue_mutex)
+    );
+    std::thread painter(
+        startPainterWorker, std::ref(data_queue), std::ref(data_queue_mutex)
+    );
+
+    speaker.join();
+    listener.join();
+    painter.join();
+}
